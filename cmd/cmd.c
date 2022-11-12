763397ccdc129dfcd7d90200b1971c0e2f3a61de
XENON <54139784+XEN486@users.noreply.github.com>
2022-11-12 19:33:12 +0000

    remove easter eggs because os is now open source
    sad

--- cmd/cmd.c
+++ cmd/cmd.c
@@ -22,10 +22,6 @@ static void stop_sound() {
 	outb(0x61, tmp);
 }
 
-void peekers() {
-	kprint("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHEY! STOP PEEKING!\nI know you are looking in the OS.bin file!\nJust stop bro!\nCMONNN you are ruining the secrets :(\nHow did you even discover this? IF I TOLD NO ONE HUH!?\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nstop\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-}
-
 int getCmdShouldLoad() {
 	if (cmd == 1) {
 		return 1;
@@ -42,22 +38,18 @@ void cmd_init() {
 }
 
 void cmd_input(char *input) {
-	
 	char todo[] = "This isn't implemented yet. Stop asking, it will be.\n";
-	if (strcmp(input, "HALT") == 0) { // halt
-        kprint("Stopping the CPU. Bye!\n");
-        halt();
-    } else if (strcmp(input, "HELP") == 0) { // help
+    	if (strcmp(input, "HALT") == 0) { // halt
+        	kprint("Stopping the CPU. Bye!\n");
+        	halt();
+    	} else if (strcmp(input, "HELP") == 0) { // help
 		kprint("-=-=Commands=-=-\nHALT - halt the cpu\nHELP - display this help dialog\nCLEAR - clear the screen\nDIR - list directories\nEDIT - edit a file\nCAT - read file\nEXECUTE - execute file\nDEMO - first program made ever\nUPDATENOTES - notes about the update\n");
 	} else if (strcmp(input, "CLEAR") == 0) { // clear
 		clear_screen();
 	} else if (strcmp(input, "CLS") == 0) { // clear
 		clear_screen();
-	} else if (strcmp(input, "THISISTHEBESTOSIVEEVERSEENTHISISSOTRUE") == 0) { // easter egg
-		kprint("CREDITS:\nDeveloper - XENON\nLead Developer - XENON\nBest Developer - XENON\nDedicated to: XENON\nTutorial from cfenollosa\nGoodbye CPU!\n");
-		halt();
-	} else if (strcmp(input, "WHATISIT") == 0) { // whatisit
-		kprint("It's the lenor dream team! /j\nI'll give you a hint. This is the BEST OS I've ever seen! This is so true!!\n");
+	} else if (strcmp(input, "CREDITS") == 0) { // easter egg
+		kprint("CREDITS:\nDeveloper - XENON\nTutorial from cfenollosa\nGoodbye CPU!\n");
 		halt();
 	} else if (strcmp(input, "DIR") == 0) { // dir (todo)
 		kprint(todo);
@@ -75,14 +67,6 @@ void cmd_input(char *input) {
 		kprint("PUCUX 0.3\n");
 	} else if (strcmp(input, "TESTCRASH") == 0) {
 		kernel_crash("TEST_CRASH");
-	} else if (strcmp(input, "GIVEMEASECRET") == 0) {
-		kprint("An alphabet!\nCXDZIQAVEPYBGHWJTKLFMOSNRU\nOh and..\nEEWHATDOESITSTANDFOREMOTIONENGINEOREASTEREGG\n");
-	} else if (strcmp(input, "IISVCFZWILEFLFCHZQWKIGWFEWHIHAEHIWKICLFIKIAA") == 0) {
-		kprint("You found the easter egg!\nWell done!\nI don't really know what to give you now..\nHmm.. try running PVFTMK!\n");
-	} else if (strcmp(input, "PVFTMK") == 0) {
-		kprint("Hah! you did it! You will get 1 secret debugging command!\nRun TESTCRASH!\n");
-	} else if (strcmp(input, "VERYBIGPEEKERWTFFFFFFFFFFFFFFFFFFFFFFFF OK SERIOUSLY JUST, STOP PEEKING AT THIS SHIT BROOOOO I'M GONNA GO INSANE FROM PEOPLE LIKE YOU TRYING TO READ EASTER EGGS LIKE THIS AND LIKE FUKASDUAIDFJKFHK") == 0) {
-		peekers();
 	} else if (strcmp(input, "SOUND") == 0) {
 		play_sound(1000);
 		kprint("That probably sounds really bad\n");
